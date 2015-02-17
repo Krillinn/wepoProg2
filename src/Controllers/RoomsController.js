87a4244fdeb9e9557447f9f62a1944aa5f1e1242
gunnargylfa <gunnargylfa@gmail.com>
2015-02-17 12:59:23 +0000

    app.js split into controllers

---
+++ src/Controllers/RoomsController.js
@@ -0,0 +1,22 @@
+ChatClient.controller('RoomsController', function ($scope, $location, $rootScope, $routeParams, socket) {
+
+	$scope.roomName = '';
+	$scope.errorMessage = '';
+	$scope.rooms = ['room 1', 'room2'];
+
+	$scope.currentUser = $routeParams.user;
+
+	$scope.submitRoomName = function() {
+		if($scope.roomName === '') {
+			$scope.errorMessage = 'Please choose a lobby-name before continuing!';
+		} else {
+			$scope.rooms.push($scope.roomName);
+		}
+	}
+
+	socket.emit('joinroom',$scope) 
+
+
+
+	);
+});
